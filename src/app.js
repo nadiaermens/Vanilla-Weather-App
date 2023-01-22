22ecb5610f3344a881872d0fbdb72eaab66c66a8
nadiaermens <83193392+nadiaermens@users.noreply.github.com>
2023-01-22 11:02:19 -0800

    added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -22,21 +22,29 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", `response.data.condition.description`);
 }
 
 let apiKey = "d481t33b8fce7a1cae73b05db657o824";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Vancouver&key=${apiKey}&units=metric`;
+let city = "Vancouver";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
